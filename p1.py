7f570bb34323a777e2d7019c2b97f146912948b7
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-07-02 20:19:19 +0900

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# project: p1
+
+
+# In[2]:
+
+
+#q1
+name = "World"
+"Hello " + name
+
+
+# In[3]:
+
+
+#q2
+6*7
+
+
+# In[4]:
+
+
+"oops"
+
+
+# In[ ]:
+
+
+
+
